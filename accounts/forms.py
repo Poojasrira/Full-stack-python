6bdce99c8ecd04b93e9e3b0c632eea7a0a8ef692
pooja <sujathapooja21@gmail.com>
2026-07-22 09:15:51 +0530

    21-07-26

--- accounts/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForms
-class LoginForm(AuthenticationForms):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    
